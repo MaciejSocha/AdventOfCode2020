0369a49e75043de6e5c03a18fcfdb7f4f5082a9a
MaciejSocha <maciek.socha1@gmail.com>
2020-12-03 00:08:52 +0100

    Day2_part1 solution

---
+++ src/main/java/day02/PasswordRecord.java
@@ -0,0 +1,30 @@
+package day02;
+
+public class PasswordRecord {
+    private int minOccurs;
+    private int maxOccurs;
+    private char character;
+    private String password;
+
+    public PasswordRecord(String oneDBLine) {
+        oneDBLine = oneDBLine.trim();
+        String occurs = oneDBLine.split(" ")[0];
+        String letter = oneDBLine.split(" ")[1];
+        String passwd = oneDBLine.split(" ")[2];
+
+        this.minOccurs = Integer.parseInt(occurs.split("-")[0]);
+        this.maxOccurs = Integer.parseInt(occurs.split("-")[1]);
+
+        this.character = letter.split(":")[0].charAt(0);
+
+        this.password = passwd;
+    }
+
+    public boolean checkIsPasswordValid() {
+        int counter = 0;
+        for (char c : password.toCharArray()) {
+            if (c == character) counter++;
+        }
+        return (counter >= minOccurs && counter <= maxOccurs);
+    }
+}
--- src/main/java/day02/Run.java
+++ src/main/java/day02/Run.java
@@ -3,11 +3,18 @@ package day02;
 import utils.ReadFile;
 
 public class Run {
+    public static int counter = 0;
+
     public static void main(String[] args) {
         String input = ReadFile.read("src/main/java/day02/input.txt");
         String tmp = "";
+
         for (int i = 0; i < input.lines().count(); i++) {
-            tmp = (String)input.lines().toArray()[i];
+            tmp = (String) input.lines().toArray()[i];
+            PasswordRecord passwdRc = new PasswordRecord(tmp);
+            if (passwdRc.checkIsPasswordValid()) counter++;
         }
+
+        System.out.println("Number of valid passwords: " + counter);
     }
 }
